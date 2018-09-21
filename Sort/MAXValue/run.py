0e2591f485378202565bec6ca4d5ff606656b826
Seongchuel Ahn <aciddust20@gmail.com>
2018-09-21 11:52:22 +0900

    bug fixed

--- Sort/MAXValue/run.py
+++ Sort/MAXValue/run.py
@@ -1,14 +1,27 @@
 answer=''
-order=[]
-numbers=[30,3,34,5,9]
-numbers.sort(reverse=True)
+numbers=[6,10,2]
+sorted(numbers, key=lambda x:x)
+for i in range(0, len(numbers)//2):
+  for j in range(i+1,len(numbers)):
+	if len(str(numbers[j])) == len(str(numbers[i])):
+	  if numbers[j] % numbers[i] == 0:
+		numbers[j] , numbers[i] = numbers[i], numbers[j]
+	else:
+	  pass
+while(len(numbers)):
+  answer+=str(numbers.pop())
+print(answer)
+
+# EX2
+"""
 for n in numbers:
   if len(str(n))>1:
 	div = len(str(n))-1
 	n = float(float(n)/(10**div))
   order.append(n)
+order.sort(reverse=True)
 print(order)
-order.sort()
 while(len(order)):
   answer+=str(order.pop()).replace('.','')
 print(answer)
+"""
---
+++ Sort/MAXValue/run2.py
@@ -0,0 +1,16 @@
+num=[3,30,34,5,9]
+
+def solution(numbers):
+  length = list()
+  temp = list()
+  for num in numbers:
+    temp.append(str(num))
+  for num in temp:
+    length.append(len(num))
+  maxlen = max(length)
+  temp.sort(key=lambda x: x*(maxlen//len(str(x))+1),reverse=True)
+  print(length)
+  print(maxlen)
+  print(temp)
+  return str(''.join(temp))
+print(solution(num))
