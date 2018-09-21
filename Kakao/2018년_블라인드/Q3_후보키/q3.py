3945e07b387a48b0cf23e3c1183a31c958374f9f
Seongchuel Ahn <aciddust20@gmail.com>
2018-09-21 17:23:35 +0900

    후보키를 판별할 수 있도록 로직작성

---
+++ Kakao/2018년_블라인드/Q3_후보키/q3.py
@@ -0,0 +1,29 @@
+﻿# -*- coding:utf-8 -*-
+from collections import OrderedDict
+relation = [
+            [100,'ryan','music',2],
+            [200,'apeach','math',2],
+            [300,'tube','computer',3],
+            [400,'con','computer',4],
+            [500,'muzi','music',3],
+            [600,'apeach','music',2]
+            ]	
+
+def solution(relation):
+    t_size = 0
+    columns = 0
+    count_idx = []
+    relation = list(zip(*relation))
+    t_size = len(relation[0])
+    columns = len(relation)
+    
+    for i in range(columns):
+        relation[i] = list(set(relation[i]))
+        count_idx.append(len(relation[i]))
+    
+    print(count_idx)
+
+    answer = 0
+    return answer
+
+print(solution(relation))
