ffa8aa1e884a6e3d12bdff4e61325f33e034bdfa
Seongchuel Ahn <aciddust20@gmail.com>
2018-09-21 17:35:48 +0900

    작성중

--- Kakao/2018년_블라인드/Q3_후보키/q3.py
+++ Kakao/2018년_블라인드/Q3_후보키/q3.py
@@ -1,10 +1,9 @@
 ﻿# -*- coding:utf-8 -*-
-from collections import OrderedDict
 relation = [
-            [100,'ryan','music',2],
+            [200,'ryan','music',2],
             [200,'apeach','math',2],
             [300,'tube','computer',3],
-            [400,'con','computer',4],
+            [200,'con','computer',4],
             [500,'muzi','music',3],
             [600,'apeach','music',2]
             ]	
@@ -19,10 +18,12 @@ def solution(relation):
     
     for i in range(columns):
         relation[i] = list(set(relation[i]))
-        count_idx.append(len(relation[i]))
+        count_idx.append( [i,len(relation[i])] )
     
-    print(count_idx)
+    sorted(count_idx, key=lambda x:x[0])
 
+    print(count_idx)
+	
     answer = 0
     return answer
 
