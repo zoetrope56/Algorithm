dc6d13157431a4a0b77d87fab0d18af6e6107b19
Seongchuel Ahn <aciddust20@gmail.com>
2018-10-11 09:28:47 +0900

    easy

--- Study/COS_PRO_1/10_initial_code.py
+++ Study/COS_PRO_1/10_initial_code.py
@@ -1,11 +1,11 @@
 def solution(prices):
     INF = 1000000001;
     tmp = INF
-    answer = -INF
+    answer = -INF 
     for price in prices:
-        if tmp != INF:
-            answer = max(answer, tmp - price)
-        tmp = min(tmp, price)
+        if tmp != INF:  
+            answer = max(answer, price - tmp) 
+        tmp = min(tmp, price) 
     return answer
 
 prices1 = [1, 2, 3];
