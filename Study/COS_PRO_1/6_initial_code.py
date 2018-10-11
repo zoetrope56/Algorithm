eaed9ee07f40101040c70309a81e4efdb4e44318
Seongchuel Ahn <aciddust20@gmail.com>
2018-10-11 09:59:42 +0900

    easy

--- Study/COS_PRO_1/6_initial_code.py
+++ Study/COS_PRO_1/6_initial_code.py
@@ -1,14 +1,28 @@
-#다음과 같이 import를 사용할 수 있습니다.
-#import math
-
 def solution(pos):
-    # 여기에 코드를 작성해주세요.
-    answer = 0
-    return answer
+  answer = 0
+  index = {"A":1, "B":2, "C":3, "D":4, "E":5, "F":6, "G":7, "H":8}
+  print(pos)
+  pos = [index[pos[0]], int(pos[1])]
+  idx = [0, 1]
+  for i in range(1, 3):
+    if i % 2 == 0:
+      idx[0], idx[1] = idx[1], idx[0]
+    
+    if pos[idx[0]]+2 < 9:
+      if pos[idx[1]]-1 > 0:
+        answer+=1
+      if pos[idx[1]]+1 < 9:
+        answer+=1
 
-#아래는 테스트케이스 출력을 해보기 위한 코드입니다.
+    if pos[idx[0]]-2 > 0:
+      if pos[idx[1]]-1 > 0:
+        answer+=1
+      if pos[idx[1]]+1 < 9:
+        answer+=1
+
+  return answer
+
 pos = "A7"
 ret = solution(pos)
 
-#[실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-print("solution 함수의 반환 값은", ret, "입니다.")
+print( ret )
